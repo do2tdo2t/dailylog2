d7ebd35c03cf8df407835a8c2f3f2b73191e8989
Park Hyojung <hj__park@naver.com>
2020-03-03 18:08:48 +0900

    init project

---
+++ dailylog2/src/test/java/com/rc/dailylog2/Dailylog2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rc.dailylog2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dailylog2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
