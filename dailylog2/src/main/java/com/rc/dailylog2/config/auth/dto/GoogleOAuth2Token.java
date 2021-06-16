704b6991da0844b3620944c193e88f3475d5d9bf
do2tdo2t <donot234@naver.com>
2021-06-16 16:19:34 +0900

    Google Calendar Api (unfinished)
    Google Calendar Api (unfinished)

--- dailylog2/src/main/java/com/rc/dailylog2/config/auth/dto/GoogleOAuth2Token.java
+++ dailylog2/src/main/java/com/rc/dailylog2/config/auth/dto/GoogleOAuth2Token.java
@@ -2,21 +2,33 @@ package com.rc.dailylog2.config.auth.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@NoArgsConstructor
 @Getter
-public class GoogleOAuth2Token {
+@ToString
+public class GoogleOAuth2Token{
     private String redirectUri;
     private String clientId;
     private String projectId;
-    private String clientSecret;
+    private String accessToken;
+    private String refreshToken;
+    private String scope;
+    private String tokenType;
+    private String idToken;
 
     @Builder
-    public GoogleOAuth2Token(String redirectUri, String clientId, String clientSecret
-            , String projectId)
+    public GoogleOAuth2Token(String redirectUri, String clientId, String accessToken
+            , String refreshToken, String scope, String tokenType, String idToken)
     {
         this.redirectUri = redirectUri;
         this.clientId = clientId;
-        this.clientSecret = clientSecret;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.scope = scope;
+        this.tokenType = tokenType;
+        this.idToken = idToken;
         this.projectId = projectId;
 
     }
--- dailylog2/src/main/java/com/rc/dailylog2/web/GoogleOAuthController.java
+++ dailylog2/src/main/java/com/rc/dailylog2/web/GoogleOAuthController.java
@@ -1,6 +1,22 @@
 package com.rc.dailylog2.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.services.calendar.model.Event;
+import com.google.api.services.calendar.model.Events;
 import com.rc.dailylog2.config.auth.dto.GoogleOAuth2Token;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,57 +30,125 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.security.GeneralSecurityException;
+import java.util.*;
 
 @Controller
 public class GoogleOAuthController {
 
-    private String tokenFile ="/tokens/credentials.json";
+    private static final String TOKEN_FILE_PATH="/credentials.json";
+    private static final String TOKEN_DIRECTORY_PATH = "/tokens";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-    @GetMapping("/dailylog2/google/auth")
+    private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
+
+
+    @GetMapping("/dailylog2/google/auth2")
     public void googleSignCallback(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         //HTTP Request를 위한 RestTemplate
-
+        //URL url = getClass().getResource(tokenFile);
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         String code = request.getParameter("code");
 
         //read token
-        ObjectMapper mapper = new ObjectMapper();
-        GoogleOAuth2Token token = mapper.readValue(new File(tokenFile), GoogleOAuth2Token.class);
-
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         MultiValueMap<String, Object > parameters = new LinkedMultiValueMap<>();
         parameters.add("code", code);
-        parameters.add("client_id", token.getClientId());
-        parameters.add("client_secret", token.getClientSecret());
-        parameters.add("redirect_uri", token.getRedirectUri());
+        parameters.add("client_id", "1069657257430-jfoqv6ue9q90osafvh09e5ga78tg9n1e.apps.googleusercontent.com");
+        parameters.add("client_secret", "kCT5-IlSH8Hq6k2p8Bt6Y3t8");
+        parameters.add("redirect_uri", "http://localhost:9101/dailylog2/google/auth");
         parameters.add("grant_type", "authorization_code");
 
         HttpEntity<MultiValueMap<String,Object>> restRequest = new HttpEntity<MultiValueMap<String,Object>>(parameters,headers);
 
         URI uri = URI.create("https://www.googleapis.com/oauth2/v4/token");
 
+        //get access_token
         ResponseEntity<String> restReponse = restTemplate.postForEntity(uri,parameters,String.class);
 
         String bodys = restReponse.getBody();
-        System.out.println("예제 테스트 ....");
-        System.out.println(bodys);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> map = new HashMap<>();
+        map = objectMapper.readValue(bodys, new TypeReference<Map<String, Object>>() {
+        });
 
-        response.sendRedirect("month-one");
+        //암호화해서 -> DB 저장
+        GoogleOAuth2Token googleOAuth2Token = GoogleOAuth2Token.builder()
+                        .accessToken((String)map.get("accessToekn"))
+                        .refreshToken((String)map.get("refreshToken"))
+                        .build();
+
+        System.out.println(googleOAuth2Token.toString());
+        // calendar 연동
+
+        //response.sendRedirect("month-one");
     }
 
-    public void requestGoogleOAuth(HttpServletRequest request, HttpServletResponse response)throws IOException {
-        String url = "https://accounts.google.com/o/oauth2/v2/auth?"
-                +"client_id="
-                +"&redirect_uri=http://localhost:9101/dailylog2/google/auth"
-                +"&response_type=code"
-                +"&scope=email%20profile%20openid"
-                +"&access_type=offline";
+    @GetMapping("/dailylog2/google/auth")
+    public void googleSignCallbackSdk() throws IOException, GeneralSecurityException {
+        System.out.println("start..googleSignCallbackSdk..");
+        final NetHttpTransport netHttpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+        com.google.api.services.calendar.Calendar service =
+                new Calendar.Builder(netHttpTransport,JSON_FACTORY,getCredentials(netHttpTransport))
+                .setApplicationName("SPRING_BOOT")
+                .build();
+
+
+        System.out.println("build..googleSignCallbackSdk..");
+        DateTime now = new DateTime(System.currentTimeMillis());
+
+        Events events = service.events().list("primary")
+                    .setMaxResults(10)
+                    .setTimeMin(now)
+                    .setOrderBy("startTime")
+                .setSingleEvents(true).execute();
+
+        List<Event> items = events.getItems();
+        if(items.isEmpty()) {
+            System.out.println("No upcoming events found.");
+        }else{
+            System.out.println("Upcoming events");
+            for(Event event:items){
+                DateTime start = event.getStart().getDateTime();
+                if(start == null){
+                    start = event.getStart().getDate();
+                }
+                System.out.printf("%s (%s)\n" ,event.getSummary(),start);
+            }
+        }
     }
 
+
+    private static Credential getCredentials(final NetHttpTransport netHttpTransport) throws IOException{
+
+        InputStream inputStream = GoogleOAuthController.class.getResourceAsStream(TOKEN_DIRECTORY_PATH+TOKEN_FILE_PATH);
+        if(inputStream == null) {
+            throw new FileNotFoundException("Resource not found: "+ TOKEN_DIRECTORY_PATH + TOKEN_FILE_PATH);
+        }
+
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(inputStream));
+      //  clientSecrets.set("redirect_uri","http://localhost:9101/dailylog2/google/auth");
+        System.out.println(clientSecrets.toPrettyString());
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                netHttpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("accessTokens")))
+                .setAccessType("offline")
+                .build();
+
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        Credential credential = new AuthorizationCodeInstalledApp(flow,receiver).authorize("user");
+
+        return credential;
+    }
+    
 }
